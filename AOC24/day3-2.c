95c173af18d546440f89b0cc7a13b94d795bd64a
KimonSMN <kimonsmirlianos@gmail.com>
2025-03-03 20:31:15 +0200

    day3 part 2 works with test input, not with puzzle input

--- AOC24/day3-2.c
+++ AOC24/day3-2.c
@@ -4,34 +4,6 @@
 #include <string.h>
 
 
-/* 
-
-flag = true;
-
-if(ptr < dontPtr){
-    flag is true 
-} 
-
-if(ptr > dontPtr) AND (ptr < doPtr){
-    flag false;
-}
-
-if (ptr > doPtr) AND (doPtr > dontPtr){
-    flag is true
-    dontPtr += 7
-}
-
-if(dontPtr > doPtr){
-    doPtr += 4
-
-}
-
-
-
-*/
-
-
-
 void findMul(char *line, int *res, bool *flag){
     int x = 0,y = 0;
     char bufferX[4096];
@@ -41,10 +13,9 @@ void findMul(char *line, int *res, bool *flag){
     char *doPtr = strstr(line, "do()");
 
     while((ptr = strstr(ptr, "mul(")) != NULL){ // while we find same string in line as "mul("
-        // dontPtr = strstr(dontPtr, "don't()"); // we find "don't()"
-        // doPtr = strstr(doPtr, "do()"); // we find "do()"
-        if (dontPtr == NULL) dontPtr = ptr + strlen(ptr); // set it far away if not found
-        if (doPtr == NULL) doPtr = ptr + strlen(ptr);     // set it far away if not found
+
+        if(dontPtr == NULL) dontPtr = ptr + strlen(ptr);
+        if(doPtr == NULL) doPtr = ptr + strlen(ptr);
 
         if(ptr < dontPtr){
             *flag = true;
@@ -56,31 +27,29 @@ void findMul(char *line, int *res, bool *flag){
         
         if ((ptr > doPtr) && (doPtr > dontPtr)){
             *flag = true;
-            dontPtr = strstr(dontPtr + 7, "don't()"); // find next occurrence safely
+            dontPtr = strstr(dontPtr + 7, "don't()");
         }
         
         if(dontPtr > doPtr){
-            doPtr += 4;
+            doPtr = strstr(doPtr + 4, "do()");
         }
             
         // if flag is true get the mul...
         if(sscanf(ptr, "mul(%d, %d)", &x, &y) == 2 && *flag){    // pass the values to x,y 
-        // convert int to string
-        sprintf(bufferX, "%d", x);
+            sprintf(bufferX, "%d", x);
             sprintf(bufferY, "%d", y);
 
             ptr += 4 + strlen(bufferX) + strlen(bufferY) + 1; // move the pointer to the closing bracket
             if(*ptr == ')'){   // if curr == ')'
                 printf("mul(%s,%s)\n", bufferX, bufferY);
                 *res += x * y; // Update result
-                ptr -= 4 + strlen(bufferX) + strlen(bufferY) + 1; // move the pointer to the closing bracket
+                ptr -= 4 + strlen(bufferX) + strlen(bufferY) + 1;
             }             
         }
         ptr += 4; // search further
     }
 
 }
-
 
 int main(){
     FILE *fp = fopen("../puzzleInput/day3.txt","r");
@@ -92,7 +61,7 @@ int main(){
     int len = ftell(fp);
     fseek(fp, 0, SEEK_SET);
     
-    char line[4096];
+    char line[10000];
     int res = 0;
     bool flag = true;
     while(fgets(line, sizeof(line), fp)){ // for every line of file.
